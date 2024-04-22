931f6d35778005b8a1c93f94b69f052e5c2b5104
0xhsn <mail@7asan.io>
2024-04-22 18:08:10 +0200

    feat: home ui

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -28,6 +28,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle } from "lucide-react"
 
 import dayjs from "dayjs";
 
@@ -50,7 +53,7 @@ export default function Home() {
   const { data: postsData, loading: loadingPosts } = useGetPostsQuery({
     variables: {
       limit: 10,
-    }
+    },
   });
 
   console.log("posts", postsData);
@@ -87,8 +90,8 @@ export default function Home() {
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-          <Button variant="ghost" className="text-xl">
-            @{data.me.username}
+            <Button variant="ghost" className="text-xl">
+              @{data.me.username}
             </Button>
           </NavigationMenuItem>
           <NavigationMenuItem>
@@ -133,10 +136,8 @@ export default function Home() {
         {body}
       </div>
       <div className="font-mono">
-        {postsData?.posts.map(
-          (
-            post: Post // Accessing the 'posts' array
-          ) => (
+        {postsData && postsData.posts.length > 0 ? (
+          postsData.posts.map((post: Post) => (
             <Card className="w-[350px] mb-4" key={post.title}>
               <CardHeader>
                 <CardTitle>{post.title}</CardTitle>
@@ -148,7 +149,39 @@ export default function Home() {
               </CardHeader>
               <CardContent>{post.text}</CardContent>
             </Card>
-          )
+          ))
+        ) : !postsData && loadingPosts ? (
+          <div className="flex flex-col justify-between space-y-6">
+            <div className="flex flex-col space-y-3">
+              <Skeleton className="h-[125px] w-[350px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[350px]" />
+                <Skeleton className="h-4 w-[350px]" />
+              </div>
+            </div>
+            <div className="flex flex-col space-y-3">
+              <Skeleton className="h-[125px] w-[350px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[350px]" />
+                <Skeleton className="h-4 w-[350px]" />
+              </div>
+            </div>
+            <div className="flex flex-col space-y-3">
+              <Skeleton className="h-[125px] w-[350px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[350px]" />
+                <Skeleton className="h-4 w-[350px]" />
+              </div>
+            </div>
+          </div>
+        ) : true && (
+          <Alert variant="destructive">
+            <AlertCircle className="h-4 w-4" />
+            <AlertTitle>Error fetching posts</AlertTitle>
+            <AlertDescription>
+              Query failed or no posts available.
+            </AlertDescription>
+          </Alert>
         )}
       </div>
     </main>
